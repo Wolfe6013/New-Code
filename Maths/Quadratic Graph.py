01bcb14f1aff719d2d652aed3ded75edb00e730c
Wolfe6013 <Wolfe6013@gmail.com>
2023-09-22 10:45:04 +0930

    Update Quadratic Graph.py

--- Maths/Quadratic Graph.py
+++ Maths/Quadratic Graph.py
@@ -3,9 +3,40 @@ import random
 
 turt = turtle.Turtle()
 turt.speed(0)
+coloursList = ['red','orange','green','blue','purple','#ff4081']
 
 def grid():    ###----- Makes Grid -----###
-    turt.color('light gray')
+    turt.color('#d4d7d8') ###light gray##
+    gridX = -400
+    while gridX <= 400:
+        turt.up()
+        turt.goto(gridX, -400)
+        turt.down()
+        turt.goto(gridX, 400)
+        gridX+=10
+    gridY = -400
+    while gridY <= 400:
+        turt.up()
+        turt.goto(-400, gridY)
+        turt.down()
+        turt.goto(400, gridY)
+        gridY+=10
+    turt.color('#696e70') ###gray##
+    gridX = -400
+    while gridX <= 400:
+        turt.up()
+        turt.goto(gridX, -400)
+        turt.down()
+        turt.goto(gridX, 400)
+        gridX+=50
+    gridY = -400
+    while gridY <= 400:
+        turt.up()
+        turt.goto(-400, gridY)
+        turt.down()
+        turt.goto(400, gridY)
+        gridY+=50
+    turt.color('#000000') ##black###
     turt.up()
     turt.goto(-400, 0)
     turt.down()
@@ -19,7 +50,12 @@ def parabola():    ###----- Makes Parabola -----###
     w  = float(input('y = '))
     h = float(input('y = w(x + '))
     l = float(input('y = w(x + h)^2 + '))
+    colour = input('Colour of line? ')
+    if colour == '':
+        colour = random.choice(coloursList)
 
+    turt.color(colour)
+    
     x = 0-h
     y = 0+l
 
@@ -27,7 +63,7 @@ def parabola():    ###----- Makes Parabola -----###
     turt.goto(x*10, y*10)
     turt.down()
 
-    while y < 50:
+    while y < 50 and y > -50:
         y = w*(x+h)**2+l
         turt.goto(x*10,y*10)
         x += 0.1
@@ -38,16 +74,17 @@ def parabola():    ###----- Makes Parabola -----###
     turt.goto(x*10, y*10)
     turt.down()
 
-    while y < 50:
+    while y < 50 and y > -50:
         y = w*(x+h)**2+l
         turt.goto(x*10,y*10)
         x -= 0.1
 
+grid()
 while True:
-    grid()
     parabola()
     spat = input()
-    if spat == '':
+    if spat == 'clear':
         turt.clear()
+        grid()
     if spat == 'exit':
         break
