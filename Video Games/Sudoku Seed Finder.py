e158d3c200378ef15c9fa871a6f5487e93cf76a6
Wolfe6013 <Wolfe6013@gmail.com>
2023-09-28 11:50:33 +0930

    Update Sudoku Seed Finder.py
    Changed while statement to a for in range statement.

--- Video Games/Sudoku Seed Finder.py
+++ Video Games/Sudoku Seed Finder.py
@@ -1,20 +1,23 @@
 import random
-done = 0
-boxChoices = [' ',' ',' ',' ',' ',' ',' ',' ',' ','1','2','3','4','5','6','7','8','9']
 
-finalSeed = []
-splt = 0
+def MakeSeed():
+    done = 0
+    boxChoices = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','1','2','3','4','5','6','7','8','9']
 
-while splt < 81:
-    while done < 81:
-        done+=1
-        seedDigit = 0
-        finalSeed.append(random.choice(boxChoices))
-    for x in boxChoices:
-        if x in finalSeed:
-            splt += 1
-            
-            
+    while True:
+        allNumbers = 0
+        finalSeed = []
+        for x in range(81):
+            finalSeed.append(random.choice(boxChoices))
+        for x in boxChoices:
+            if x in finalSeed:
+                allNumbers += 1
+        if allNumbers == len(boxChoices):
+            break
 
-for x in finalSeed:
-    print(x, end-'')
+    for x in finalSeed:
+        print(x, end='')
+
+while True:
+    MakeSeed()
+    input()
