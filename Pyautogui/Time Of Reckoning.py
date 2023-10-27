c0b6b00e34677048bc2feeb33228ce2a5386d3de
Wolfe6013 <Wolfe6013@gmail.com>
2023-10-27 20:47:03 +1030

    Update Time Of Reckoning.py

--- Pyautogui/Time Of Reckoning.py
+++ Pyautogui/Time Of Reckoning.py
@@ -4,7 +4,7 @@ import time
 
 time.sleep(3)
 while not keyboard.is_pressed('`'):
-    text_to_type = "Hello mother!"
+    text_to_type = "Yap yap yap"
     for letter in text_to_type:
         pyautogui.typewrite(letter)
     time.sleep(1)
