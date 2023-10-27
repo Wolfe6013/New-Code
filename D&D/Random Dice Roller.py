5b83cd7b4351ef5fb1bd3bf5355bc6613055d624
Wolfe6013 <Wolfe6013@gmail.com>
2023-10-27 20:46:38 +1030

    Update Random Dice Roller.py

--- D&D/Random Dice Roller.py
+++ D&D/Random Dice Roller.py
@@ -2,6 +2,6 @@ import random
 import time
 
 while True:
-    dice = int(input("How many sides are on the dice?  "))
+    dice = int(input("How many sides are on the dice? "))
     num = random.randint(1,dice)
     print("You got",num,)
