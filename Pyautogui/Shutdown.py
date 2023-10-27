ea9a3d08e98815830bf3702c1ecd9411731320fe
Wolfe6013 <Wolfe6013@gmail.com>
2023-10-27 20:46:58 +1030

    Update Shutdown.py

--- Pyautogui/Shutdown.py
+++ Pyautogui/Shutdown.py
@@ -1,7 +1,14 @@
+import pygetwindow as gw
 import pyautogui
 import time
+
+open_windows = gw.getWindowsWithTitle('')
+tab_count = len(open_windows)
 
 pyautogui.click(20, 1048)
 pyautogui.click(20, 1000)
 pyautogui.click(20, 877)
-pyautogui.click(button='left', clicks=10, interval=0.5)
+pyautogui.click(20, 877)
+if tab_count > 0:
+    for window in open_windows:
+        pyautogui.hotkey("alt", "f4")
