3f7674d66eb5f8480b72c73d45cff552e9e71f31
Wolfe6013 <Wolfe6013@gmail.com>
2023-11-20 22:24:45 +1030

    Update Tab Counter.py

--- Pyautogui/Tab Counter.py
+++ Pyautogui/Tab Counter.py
@@ -1,13 +1,12 @@
 import pygetwindow as gw
 
-# Get a list of all open windows (tabs)
 open_windows = gw.getWindowsWithTitle('')
-tab_count = len(open_windows)
+file_count = len(open_windows)
 
-if tab_count > 0:
-    print(f"Number of open tabs: {tab_count}")
-    print("List of open tabs:")
+if file_count > 0:
+    print(f"Number of open files: {file_count}")
+    print("List of open files:")
     for window in open_windows:
         print(window.title)
 else:
-    print("No open tabs found.")
+    print("No open files found.")
