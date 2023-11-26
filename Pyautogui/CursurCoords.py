b3e3cac5bdc5bb51df51c748f86a0c464b261838
Wolfe6013 <Wolfe6013@gmail.com>
2023-11-26 21:57:21 +1030

    Update CursurCoords.py

--- Pyautogui/CursurCoords.py
+++ Pyautogui/CursurCoords.py
@@ -1,14 +1,8 @@
 import pyautogui
 import time
 
-print("3...")
-time.sleep(1)
-print("2...")
-time.sleep(1)
-print("1...")
-time.sleep(1)
-
-while True:
+def Coords():
     x, y = pyautogui.position()
     print(f"Current Cursor Coordinates: x={x}, y={y}")
-    time.sleep(1)
+
+keyboard.add_hotkey('space', Coords)
