ecaa9ec734292159d5a60aa157081e5af1c018bf
Wolfe6013 <Wolfe6013@gmail.com>
2024-03-13 12:44:55 +1030

    Create CSH Song Finder.py

---
+++ Miscellaneous/CSH Song Finder.py
@@ -0,0 +1,9 @@
+import os, sys
+
+def clear():
+    os.system("cls")
+
+TeensOfDenial = [Vincent,DestroyedByHippiePowers]
+HowToLeaveTown = [EndingOfDramamine]
+TwinFantasyFtF = [MyBoy,BeachLifeInDeath]
+Playlist = [TeensOfDenial,HowToLeaveTown,TwinFantasyFtF,TwinFantasyMtM,TeensOfStyle,Monomania,MyBackIsKillingMeBaby,NervousYoungMen,MakingADoorLessOpen,LivingWhileStarving,DisjectaMembra,Three,FacesOfTheMasquerade]
