f8d58ae489ec9189f72e5954d237cea931cb9062
Wolfe6013 <Wolfe6013@gmail.com>
2024-05-30 15:36:06 +0930

    Crytography

--- Cyphers and Codes/Matrix.py
+++ Cyphers and Codes/Matrix.py
@@ -1,25 +1,30 @@
-K: list = [[1,2],
-           [3,4]]
+K: list = [[1,0],
+           [0,1]]
 
 input: str = input("Numbers: ")
 decrypted: list[int] = input.split(" ")
 if len(decrypted) % 2 == 1:
-    decrypted.append(0)
-print(decrypted)
+    decrypted.append(f"{int(0)}")
 NumberOfMatrixes: int = int(len(decrypted)/2)
-encrypted: list[int]
+encrypted: list[int] = []
 for x in range(NumberOfMatrixes):
-    value1 = K[0][0]
-    value2 = K[0][1]
-    value3 = K[1][0]
-    value4 = K[1][1]
-    print(value1,value2,value3,value4)
-    print(f"{(int(decrypted[((2*x)-1)]*K[0][0]))+int(int(decrypted[((2*x)-1)])*int(K[1][0]))}")
-    print(f"{int(decrypted[((2*x))]*K[0][1])+int(int(decrypted[(2*x)])*int(K[1][1]))}")
-    TopMatrix = int(decrypted[(2*x)-1]*K[0][0])+int(decrypted[(2*x)-1]*(K[1][0]))
-    BottomMatrix = int(decrypted[(2*x)]*K[0][1])+int(decrypted[(2*x)-1]*(K[1][0]))
+    value1: int = K[0][0]
+    value2: int = K[0][1]
+    value3: int = K[1][0]
+    value4: int = K[1][1]
+    OneByOne: int = decrypted[(2*x)-2]*value1
+    TwoByTwo: int = decrypted[(2*x)-1]*value2
+    ThreeByOne: int = decrypted[(2*x)-2]*value3
+    FourByTwo: int = decrypted[(2*x)-1]*value4
+    TopMatrix = int(OneByOne+TwoByTwo)
+    BottomMatrix = int(ThreeByOne+FourByTwo)
     encrypted.append(TopMatrix)
     encrypted.append(BottomMatrix)
+
+encrypted.append((encrypted[0]))
+encrypted.pop(0)
+encrypted.append((encrypted[0]))
+encrypted.pop(0)
 
 for x in encrypted:
     print(f"{x} ",end="")
